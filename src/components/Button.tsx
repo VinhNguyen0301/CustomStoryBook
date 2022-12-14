06b3dbc1ddf9576f7d01b18d63e38e62a8ca4bdf
VinhNguyen <vinh.nguyen@dedoco.com>
2022-12-15 00:55:15 +0700

    Basic source

---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+import Button from "@mui/material/Button";
+import { styled } from "@mui/material/styles";
+
+const StyledButton = styled(Button)`
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border-radius: 3px;
+`;
+
+export default StyledButton;
